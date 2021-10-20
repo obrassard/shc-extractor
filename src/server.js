9b7d0fd443a8bd86b2e39bc7423eecb2b7f3cc08
Olivier Brassard <brassard.oli@gmail.com>
2021-10-20 09:00:04 -0400

    Added /json enspoint to return the raw json data from a GET request

--- src/server.js
+++ src/server.js
@@ -19,11 +19,13 @@ server.set('views', path.join(__dirname, './views'));
 server.engine('hbs', exphbs({ extname: '.hbs', defaultLayout: 'main' }));
 server.set('view engine', 'hbs');
 
+/* Return the QR code reader page */
 /* GET / */
 server.get('/', async (_, res) => {
     res.render('reader');
 });
 
+/* Get the decoded data in the web UI  */
 /* GET /card?shc=0242130.... */
 server.get('/card', async (req, res) => {
     const shcRawData = req.query.shc;
@@ -42,17 +44,26 @@ server.get('/card', async (req, res) => {
     }
 });
 
-/* POST { raw: "shc:/0242130...."} */
-server.post('/', async (req, res) => {
-    const shcRawData = req.body.raw;
+const returnJson = async (shcRawData, req, res) => {
     if (shcRawData) {
         const parsedData = await parseShc(shcRawData);
         res.json(parsedData);
     } else {
         res.status(400).send('400 Bad Request');
     }
+}
+
+/* Get the decoding result in JSON format */
+/*  GET /json?shc=0242130.... */
+server.get('/json', async (req, res) => {
+    await returnJson(req.query.shc, req, res);
+});
+
+/* POST { raw: "shc:/0242130...."} */
+server.post('/', async (req, res) => {
+    await returnJson(req.body.raw, req, res);
 });
 
 server.listen(port);
-console.log(`Listening on port :${port}`)
+console.log(`Listening on localhost:${port}`)
 
